0871a6eb6f311b12f9e944afd712f02b4bb4433c
novariawan <46988547+novariawan@users.noreply.github.com>
2019-02-15 00:23:49 +0700

    Update exercise-9.js

--- exercise-9.js
+++ exercise-9.js
@@ -11,18 +11,10 @@ function checkAB(num) {
             arrB.push(i)
         }
     } 
-       //console.log (arrA)
-       //console.log (arrB)
-
-
     for (var i = 0; i < arrA.length; i++) {
-
         for (var j = 0; j < arrB.length; j++) {
-
             var jarak= Math.abs(arrB[j]-arrA[i])
-
         }
-        //console.log (jarak)
     }
     return (jarak===4)
   // you can only write your code here!
