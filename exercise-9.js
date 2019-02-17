048473cc1a1dc25230db41693bb72cebab3a9fa5
novariawan <46988547+novariawan@users.noreply.github.com>
2019-02-17 22:11:38 +0700

    Update exercise-9.js

--- exercise-9.js
+++ exercise-9.js
@@ -1,24 +1,42 @@
 // Logic Challenge - Check AB
-function checkAB(num) {
-    var arrA= []
-    var arrB= []
+// function checkAB(num) {
+//     var arrA= []
+//     var arrB= []
 
-    for (var i = 0; i < num.length; i++) {
-        if (num[i] === 'a') {
-            arrA.push(i)
-        }else
-        if (num[i] === 'b') {
-            arrB.push(i)
-        }
-    } 
-    for (var i = 0; i < arrA.length; i++) {
-        for (var j = 0; j < arrB.length; j++) {
-            var jarak= Math.abs(arrB[j]-arrA[i])
+//     for (var i = 0; i < num.length; i++) {
+//         if (num[i] === 'a') {
+//             arrA.push(i)
+//         }else
+//         if (num[i] === 'b') {
+//             arrB.push(i)
+//         }
+//     } 
+//        //console.log (arrA)
+//        //console.log (arrB)
+
+
+//     for (var i = 0; i < arrA.length; i++) {
+
+//         for (var j = 0; j < arrB.length; j++) {
+
+//             var jarak= Math.abs(arrB[j]-arrA[i])
+
+//         }
+//         //console.log (jarak)
+//     }
+//     return (jarak===4)
+//   // you can only write your code here!
+// }
+function checkAB(num) {
+    var result = false;
+    for(var i = 0; i < num.length-4; i++){
+      if(num[i] === 'a' && num[i+4] === 'b' || num[i] === 'b' && num[i+4] === 'a') {
+        result = true;
         }
     }
-    return (jarak===4)
-  // you can only write your code here!
+return result;
 }
+  
 
 // TEST CASES
 console.log(checkAB('lane borrowed')); // true
