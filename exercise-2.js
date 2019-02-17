78f7e783f1f4fdf0b03d9909bd3ac264213058ab
novariawan <46988547+novariawan@users.noreply.github.com>
2019-02-17 22:02:21 +0700

    Update exercise-2.js

--- exercise-2.js
+++ exercise-2.js
@@ -1,14 +1,28 @@
 // Logic Challenge - Faktor Persekutuan Terbesar
 
+// function fpb(angka1, angka2) {
+// var hasil = 0;
+//     for(var i = 1; i <= angka1; i ++){
+//         if(angka1 % i === 0 && angka2 % i === 0) { 
+//             hasil = i;
+//             }
+//         } 
+// return hasil;
+//     // you can only write your code here!
+// }
 function fpb(angka1, angka2) {
-var hasil = 0;
-    for(var i = 1; i <= angka1; i ++){
-        if(angka1 % i === 0 && angka2 % i === 0) { 
-            hasil = i;
-            }
-        } 
-return hasil;
-    // you can only write your code here!
+var min = 0;
+    if(angka1 > angka2){
+        min = angka2;
+    } else {
+        min = angka1;
+    }
+  
+    for(var i = min; i>=1; i--){
+        if(angka1 % i === 0 && angka2 % i === 0){
+        return i;
+        }
+    }
 }
   
   // TEST CASES
