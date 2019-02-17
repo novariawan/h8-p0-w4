7b4ac2f9e6c8353b35860121322f36a199f34ffa
novariawan <46988547+novariawan@users.noreply.github.com>
2019-02-17 21:07:18 +0700

    Add files via upload

---
+++ exercise-13.js
@@ -0,0 +1,59 @@
+// Logic Challenge: Deep Sum
+
+function deepSum (arr) {
+    if (arr === [] || arr === [[]]){
+        return "No Number"
+    }
+    var total = 0
+    for (var x=0; x<arr.length; x++){
+        for (var y=0; y<arr[x].length; y++){
+            for (var z=0; z<arr[x][y].length; z++){
+           total += arr[x][y][z]
+          }
+        }  
+      }
+return total;
+    // Code disini
+  }
+  
+  //TEST CASE
+  console.log(deepSum([
+    [
+      [4, 5, 6],
+      [9, 1, 2, 10],
+      [9, 4, 3]
+    ],
+    [
+      [4, 14, 31],
+      [9, 10, 18, 12, 20],
+      [1, 4, 90]
+    ],
+    [
+      [2, 5, 10],
+      [3, 4, 5],
+      [2, 4, 5, 10]
+    ]
+  ])); // 316
+  
+  console.log(deepSum([
+    [
+      [20, 10],
+      [15],
+      [1, 1]
+    ],
+    [
+      [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11],
+      [2],
+      [9, 11]
+    ],
+    [
+      [3, 5, 1],
+      [1, 5, 3],
+      [1]
+    ],
+    [
+      [2]
+    ]
+  ])); // 156
+  
+  console.log(deepSum([])); // No number
